7b106a432e8c94bbf12fa83160cce8009429a212
akhilkumar332 <akhilkumar332@gmail.com>
2026-05-20 07:57:51 +0200

    fix: update dirty database resolution script to version 28

--- scripts/fix_dirty_db.sh
+++ scripts/fix_dirty_db.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker-compose exec -T db psql -U postgres -d mcp -c "UPDATE schema_migrations SET dirty = false WHERE version = 2;"
+docker-compose exec -T db psql -U postgres -d mcp -c "UPDATE schema_migrations SET dirty = false WHERE version = 28;"
